706fd272f37a7ebea0ad63658b96481629f04cab
Robin Hack <hack.robin@gmail.com>
2015-05-04 00:56:46 +0200

    libraries: MegaDrive example: Move setup out of setup() function.
    Initialize X out of setup() function.
    
    Should looks better now.

--- libraries/MegaDrive/examples/megadrive-example/megadrive-example.ino
+++ libraries/MegaDrive/examples/megadrive-example/megadrive-example.ino
@@ -1,11 +1,20 @@
 #include <MegaDrive.h>
 
 int led = 13;
-static struct mg_pin_setup ps;
-static struct mg_three_callbacks clbs;
-static struct mg_three_callbacks clbs_down;
 
-MegaDrive<mg_three_callbacks> *X;
+static struct mg_pin_setup ps = {
+  .D0 = 4, // Use pin 4 of Arduino uno as input (UP key)
+};
+
+static struct mg_three_callbacks clbs = {
+  .up_fnc = &up_keyup,
+};
+
+static struct mg_three_callbacks clbs_down = {
+  .up_fnc = &up_keydown,
+};
+
+MegaDrive<mg_three_callbacks> *X = new MegaDrive<mg_three_callbacks>(ps, clbs, clbs_down);;
 
 void up_keyup(void)
 {
@@ -19,14 +28,6 @@ void up_keydown(void)
 
 
 void setup() {
-
-  ps.D0 = 4; // Use pin 4 of Arduino uno as input (UP key)
-
-  clbs_down.up_fnc = &up_keydown;
-  clbs.up_fnc = &up_keyup;
-
-  X = new MegaDrive<mg_three_callbacks>(ps, clbs, clbs_down);
-
   pinMode(led, OUTPUT);
 }
 
