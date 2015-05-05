7d7fed8a87d08f0d51e1e9841d9190fb5de5461d
Robin Hack <hack.robin@gmail.com>
2015-05-05 12:57:14 +0200

    libraries: MegaDrive: Fix unsupported non-trivial designated structure initialization.
    Because avr-c++ doesn't support non-trivial designated structure
    initialization I must initialize all structures manually.

--- libraries/MegaDrive/examples/megadrive-example/megadrive-example.ino
+++ libraries/MegaDrive/examples/megadrive-example/megadrive-example.ino
@@ -1,38 +1,70 @@
 #include <MegaDrive.h>
 
-const int led = 13;
+const int up_led = 13;
+const int left_led = 12;
 
 static const struct mg_pin_setup ps = {
-  .D0 = 4, // Use pin 4 of Arduino uno as input (UP key)
+  .D0 = 1,     // (UP key)
+  .D1 = 2,     // (DOWN key)
+  .D2 = 3,     // (LEFT key)
+  .D3 = 4,     // (RIGHT key)
+  .D4 = 5,     // (B or A key)
+  .SELECT = 6, // (SELECT signal - output)
+  .D5 = 7,     // (C or START key)
 };
 
 static const struct mg_three_callbacks clbs_up = {
   .up_fnc = &up_keyup,
+  .down_fnc = NULL,
+  .left_fnc = &left_keyup,
+  .right_fnc = NULL,
+  .a_fnc = NULL,
+  .b_fnc = NULL,
+  .c_fnc = NULL,
+  .start_fnc = NULL
 };
 
 static const struct mg_three_callbacks clbs_down = {
   .up_fnc = &up_keydown,
+  .down_fnc = NULL,
+  .left_fnc = &left_keydown,
+  .right_fnc = NULL,
+  .a_fnc = NULL,
+  .b_fnc = NULL,
+  .c_fnc = NULL,
+  .start_fnc = NULL
 };
 
- MDThreeBtnsCallbacks *X = new  MDThreeBtnsCallbacks(ps, clbs_up, clbs_down);
+MDThreeBtnsCallbacks *const X = new  MDThreeBtnsCallbacks(ps, clbs_up, clbs_down);
+
+void left_keyup(void)
+{
+  digitalWrite(left_led, LOW);
+}
+
+void left_keydown(void)
+{
+  digitalWrite(left_led, HIGH);
+}
+
 
 void up_keyup(void)
 {
-  digitalWrite(led, LOW);
+  digitalWrite(up_led, LOW);
 }
 
 void up_keydown(void)
 {
-  digitalWrite(led, HIGH);
+  digitalWrite(up_led, HIGH);
 }
 
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(up_led, OUTPUT);
+  pinMode(left_led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   X->update_states();
   delay(20);
 }
