aa44e4f9f1304efd9274fbf363d90e40d8193c35
Robin Hack <hack.robin@gmail.com>
2015-05-05 20:48:39 +0200

    libraries: MegaDrive: examples: More rich example.

--- libraries/MegaDrive/examples/megadrive-example/megadrive-example.ino
+++ libraries/MegaDrive/examples/megadrive-example/megadrive-example.ino
@@ -2,6 +2,8 @@
 
 const int up_led = 13;
 const int left_led = 12;
+const int down_led = 11;
+const int right_led = 10;
 
 static const struct mg_pin_setup ps = {
   .D0 = 1,     // (UP key)
@@ -15,9 +17,9 @@ static const struct mg_pin_setup ps = {
 
 static const struct mg_three_callbacks clbs_up = {
   .up_fnc = &up_keyup,
-  .down_fnc = NULL,
+  .down_fnc = &down_keyup,
   .left_fnc = &left_keyup,
-  .right_fnc = NULL,
+  .right_fnc = &right_keyup,
   .a_fnc = NULL,
   .b_fnc = NULL,
   .c_fnc = NULL,
@@ -26,9 +28,9 @@ static const struct mg_three_callbacks clbs_up = {
 
 static const struct mg_three_callbacks clbs_down = {
   .up_fnc = &up_keydown,
-  .down_fnc = NULL,
+  .down_fnc = &down_keydown,
   .left_fnc = &left_keydown,
-  .right_fnc = NULL,
+  .right_fnc = &right_keydown,
   .a_fnc = NULL,
   .b_fnc = NULL,
   .c_fnc = NULL,
@@ -47,6 +49,26 @@ void left_keydown(void)
   digitalWrite(left_led, HIGH);
 }
 
+void down_keyup(void)
+{
+  digitalWrite(down_led, LOW);
+}
+
+void right_keydown(void)
+{
+  digitalWrite(right_led, HIGH);
+}
+
+void right_keyup(void)
+{
+  digitalWrite(right_led, LOW);
+}
+
+
+void down_keydown(void)
+{
+  digitalWrite(down_led, HIGH);
+}
 
 void up_keyup(void)
 {
@@ -62,9 +84,12 @@ void up_keydown(void)
 void setup() {
   pinMode(up_led, OUTPUT);
   pinMode(left_led, OUTPUT);
+  pinMode(down_led, OUTPUT);
+  pinMode(right_led, OUTPUT);
 }
 
 void loop() {
   X->update_states();
   delay(20);
 }
+
